b5609cc57f78e709f8c6cc4a0fdbfee0ea5e0142
Yoni Calsin <yonicb20@gmail.com>
2020-07-28 21:59:05 -0500

    feat: 🎸 created the navbar compnent

---
+++ components/navbar.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+
+const Navbar: FC = () => {
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="brand">
+          <h1>MyTools</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ pages/slugger/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const PageSlugger: NextPage = () => {
+  return (
+    <>
+      <h1>Hola desde la pagina de slugger</h1>
+    </>
+  );
+};
+
+export default PageSlugger;
